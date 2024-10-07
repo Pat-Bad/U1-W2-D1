d668177dc48d720dfb2293da18dbb953bb58239d
Patricia <patricia.badji89@gmail.com>
2024-10-07 15:24:15 +0200

    ex.1

---
+++ D1.js
@@ -0,0 +1,64 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+/*Stringhe, cioè concatenazioni di caratteri tra apici.
+Numeri, che possono esseri interi o decimali
+Boolean(i), cioè un valore "vero" o "falso"
+Undefined, che avrò quando definico una variabile ma non le assegno alcun valore
+Null, cioè l'esplicita assenza di valore. Lo utilizzo quando voglio esplicitamente assegnare valore di "niente" alla variabile.
+ */
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+
+*/ let myName = 'Patricia'
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/ let sum = 12 + 20
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/ let x = 12
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/ let myName = 'Badji'
+const myName = 'Patricia Badji'
+
+/* lanciando console.log(myName), il risultato è Uncaught SyntaxError: Identifier 'myName' has already been declared (at index.js:8:5) */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+let two = 4
+x = 12
+let total = two - x
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+let name1 = 'john'
+let name2 = 'John'
+name1 === name2
+
+/* SCRIVI QUI LA TUA RISPOSTA */
---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Hello World!')
+let myName = 'Patricia'
+console.log(myName)
+let sum = 12 + 20
+console.log(sum)
+let x = 12
+console.log(x)
+
+console.log(myName)
+
+let two = 4
+x = 12
+let total = two - x
+console.log(total)
+
+let name1 = 'john'
+let name2 = 'John'
+let fullname = name1 === name2
+console.log(fullname)
+fullname = name1 !== name2
+console.log(fullname)
