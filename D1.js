57a49193dc485ae677a61af179332131e8c8ca72
Patricia <patricia.badji89@gmail.com>
2024-10-07 15:26:30 +0200

    completed

--- D1.js
+++ D1.js
@@ -19,46 +19,43 @@ Null, cioè l'esplicita assenza di valore. Lo utilizzo quando voglio esplicitame
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 
-*/ let myName = 'Patricia'
+*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let myName = 'Patricia' */
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
-*/ let sum = 12 + 20
+*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let sum = 12 + 20 */
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
-*/ let x = 12
+*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let x = 12 */
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
-*/ let myName = 'Badji'
-const myName = 'Patricia Badji'
+*/
+/* let myName = 'Badji'
+const myName = 'Patricia Badji'*/
 
 /* lanciando console.log(myName), il risultato è Uncaught SyntaxError: Identifier 'myName' has already been declared (at index.js:8:5) */
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
-let two = 4
+/*let two = 4
 x = 12
-let total = two - x
-
-/* SCRIVI QUI LA TUA RISPOSTA */
+let total = two - x*/
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
-let name1 = 'john'
+/* let name1 = 'john'
 let name2 = 'John'
-name1 === name2
-
-/* SCRIVI QUI LA TUA RISPOSTA */
+name1 === name2 */
